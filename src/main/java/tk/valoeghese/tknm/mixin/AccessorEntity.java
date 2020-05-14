5a5e3095970e05d7337f2e28e696a0baf9963c7b
valoeghese <valoeghesemods@gmail.com>
2020-05-14 13:04:03 +1200

    fix

--- src/main/java/tk/valoeghese/tknm/mixin/AccessorEntity.java
+++ src/main/java/tk/valoeghese/tknm/mixin/AccessorEntity.java
@@ -1,10 +1,9 @@
 package tk.valoeghese.tknm.mixin;
 
-import javax.swing.text.html.parser.Entity;
-
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.data.DataTracker;
 
 @Mixin(Entity.class)
