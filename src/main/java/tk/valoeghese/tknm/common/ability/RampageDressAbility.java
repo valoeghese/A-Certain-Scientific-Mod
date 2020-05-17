3df003fcbc5d039d8e47bca9845d242f7acf3a72
valoeghese <valoeghesemods@gmail.com>
2020-05-17 20:45:01 +1200

    make it work I think

--- src/main/java/tk/valoeghese/tknm/common/ability/RampageDressAbility.java
+++ src/main/java/tk/valoeghese/tknm/common/ability/RampageDressAbility.java
@@ -1,26 +1,20 @@
 package tk.valoeghese.tknm.common.ability;
 
-import static tk.valoeghese.tknm.common.ability.ElectromasterAbility.CHARGE_OFF;
-import static tk.valoeghese.tknm.common.ability.ElectromasterAbility.CHARGE_ON;
-
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.Hand;
 import net.minecraft.world.World;
-import tk.valoeghese.tknm.api.ability.AbilityRenderer;
-import tk.valoeghese.tknm.api.ability.BasicAbility;
-import tk.valoeghese.tknm.client.abilityrenderer.ElectromasterAbilityRenderer;
 
-public class RampageDressAbility extends BasicAbility {
+public class RampageDressAbility extends ElectromasterAbility {
 	@Override
-	protected int[] performAbility(World world, PlayerEntity player, int level, float abilityProgress, byte usage) {
+	public int[] performAbility(World world, PlayerEntity player, int level, float abilityProgress, byte usage, ElectromasterAbility.Data data) {
 		if (level > 0) {
 			if (player.getStackInHand(Hand.MAIN_HAND).isEmpty()) {
 				boolean charged = ElectromasterAbility.CHARGED.getBoolean(player.getUuid());
 
 				if (charged) {
-					player.setMovementSpeed(player.getMovementSpeed() / 2);
+					player.setMovementSpeed(player.getMovementSpeed() / 3);
 				} else {
-					player.setMovementSpeed(player.getMovementSpeed() * 2);
+					player.setMovementSpeed(player.getMovementSpeed() * 3);
 				}
 
 				return ElectromasterAbility.performAlterCharge(world.getTime(), player, charged ? CHARGE_OFF : CHARGE_ON);
@@ -28,10 +22,5 @@ public class RampageDressAbility extends BasicAbility {
 		}
 
 		return null;
-	}
-
-	@Override
-	protected AbilityRenderer createAbilityRenderer() {
-		return new ElectromasterAbilityRenderer();
 	}
 }
